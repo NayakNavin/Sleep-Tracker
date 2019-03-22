735ac33542a17d7b72cade1f6a9e922cc6583751
Navin Nayak <navin.nayak1995@gmail.com>
2019-03-22 12:03:25 +0530

    refactor:

--- app/src/main/java/com/navinnayak/android/sleeptracker/SleepService.java
+++ app/src/main/java/com/navinnayak/android/sleeptracker/SleepService.java
@@ -3,7 +3,6 @@ package com.navinnayak.android.sleeptracker;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.icu.util.Calendar;
 import android.os.IBinder;
@@ -21,22 +20,24 @@ public class SleepService extends Service {
         // This one in particular means that if for some reason
         // this service is killed, we don't want to start it
         // again automatically
-        return START_REDELIVER_INTENT;
+
+        beginAppInBackground();
+        return START_NOT_STICKY;
     }
 
     /**
      * method to schedule the app to run at scheduled time
      **/
-    public void beginAppInBackground(Context context) {
+    public void beginAppInBackground() {
         alarmMgr = (AlarmManager) getSystemService(ALARM_SERVICE);
-        Intent intent = new Intent(context, SleepService.class);
-        alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        Intent intent = new Intent(this, SleepService.class);
+        alarmIntent = PendingIntent.getService(this, 0, intent, 0);
 
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 13);
-        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.HOUR_OF_DAY, 11);
+        calendar.set(Calendar.MINUTE, 00);
 
 
 //// With setInexactRepeating(), you have to use one of the AlarmManager interval
